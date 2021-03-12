999669af0254b3a6706820f23da9adc369967999
Firdaws Aboulaye <f.aboulaye@iplabs.de>
2021-03-12 18:04:47 +0100

    first commit

---
+++ todo-api/create-todo/app.js
@@ -0,0 +1,21 @@
+const aws = require('aws-sdk');
+const uuid = require('uuid/v4');
+
+const TABLE_NAME = process.env.TABLE_NAME;
+exports.handler = async (event, context) => {
+    try {
+        // const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello world',
+                // location: ret.data.trim()
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
